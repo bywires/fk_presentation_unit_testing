87fd3718fea98d034a49a57011dd68326b0de5a9
Bob McKee <bmckee@bywires.com>
2013-08-12 12:03:36 -0400

    added presentation with all styles

---
+++ flipkey.js
@@ -0,0 +1,23 @@
+Reveal.initialize({
+    center: true,
+    transition: 'linear',
+    overview: true,
+    progress: true,
+    rollingLinks: false,
+    width: 960,
+    height: 800,
+
+    dependencies: [
+        // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
+        { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+
+        // Syntax highlight for <code> elements
+        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+
+        // Zoom in and out with Alt+click
+        { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+
+        // Speaker notes
+        { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+    ]
+});
