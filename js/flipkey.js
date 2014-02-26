55d23912280265e7a0bfb5e0739b7f12ac634d3d
Bob McKee <bmckee@bywires.com>
2014-02-26 10:52:54 -0500

    partially completed js presentation

--- js/flipkey.js
+++ js/flipkey.js
@@ -9,15 +9,15 @@ Reveal.initialize({
 
     dependencies: [
         // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
-        { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: '/lib/js/classList.js', condition: function() { return !document.body.classList; } },
 
         // Syntax highlight for <code> elements
-        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: '/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
 
         // Zoom in and out with Alt+click
-        { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: '/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
 
         // Speaker notes
-        { src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: '/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
     ]
 });
