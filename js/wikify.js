6e81508ae021390f9a97e6b045cda7638e815611
Bob McKee <bmckee@bywires.com>
2014-02-26 16:55:52 -0500

    finished first iteration of wikifying presentations

--- js/wikify.js
+++ js/wikify.js
@@ -14,11 +14,19 @@ $(function() {
 
         $.get(url)
         .success(function(html) {
-            $('#wiki').html( $('.slides', html).html() );
+            $('#wiki').html( wikify(html) );
         })
         .error(function() {
             $('#wiki').text('Error loading presentation');
         });
+    });
+
+    $('#wiki').click(function() {
+        if(!this.firstChild) return;
+
+        var range = document.createRange();
+        range.selectNode(this);
+        window.getSelection().addRange(range);
     });
 });
 
@@ -34,4 +42,32 @@ function getPresentationLinks(html) {
     });
 
     return presentations;
+}
+
+function wikify(html) {
+    var slides = $('.slides', html)
+
+    // replace elements so they can be styled with CSS
+
+    // img's can't use :before and :after, so replace with div
+    slides.find('img').replaceWith(function() {
+        var div = $('<div>')
+            .addClass('img')
+            .attr('data-src', this.src);
+
+        return div;
+    });
+
+    // handle whitespace that will be preformatted
+    slides.find('code').html(function(index, html) {
+        return "\n" + $.trim(html) + "\n";
+    });
+
+    // pre's that don't wrap code elements
+    slides.find('pre:not(:has(>code))').addClass('pre');
+
+    // add breaks for readability
+    slides.find('h1, h2, h3, ul, ol, p, pre').after('<br>');
+
+    return slides.html();
 }
